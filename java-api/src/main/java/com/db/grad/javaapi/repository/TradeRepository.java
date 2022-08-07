11611e0da468eb8585e594393d470c77540ebae9
Ravi Jain <jainravi9915@gmail.com>
2022-08-07 15:58:23 +0530

    Changes Api

--- java-api/src/main/java/com/db/grad/javaapi/repository/TradeRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/TradeRepository.java
@@ -3,9 +3,7 @@ package com.db.grad.javaapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.db.grad.javaapi.model.Trade;
+import com.db.grad.javaapi.model.Trade ;
 
 @Repository
-public interface TradeRepository extends JpaRepository<Trade, Long>{
-
-}
+public interface TradeRepository extends JpaRepository<Trade, Long>{}
